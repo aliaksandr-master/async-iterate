c6d69f262a5563eb3e2ecff7dc50dde07ed396c5
Aliaksandr Pasynkau <alxe.master@gmail.com>
2015-01-02 10:15:01 +0300

    add tests for utils

---
+++ tests/lib/utils.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports = process.env.ASYNCITERATE_COV ? require('./../../lib-cov/utils') : require('./../../lib/utils');
---
+++ tests/utils.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var utils = require('./lib/utils');
+
+module.exports['is array'] = {
+	'valid': function (test) {
+		test.ok(utils._isArray([]));
+		test.strictEqual(utils._isArray([]), utils.isArray([]));
+		test.done();
+	},
+	'invalid': function (test) {
+		test.ok(!utils._isArray({}));
+		test.strictEqual(utils._isArray({}), utils.isArray({}));
+		test.done();
+	}
+};
+
+module.exports['keys'] = {
+	'array': function (test) {
+		test.deepEqual(utils._keys([1, 2, 3]), [0, 1, 2]);
+		test.deepEqual(utils._keys([1, 2, 3]), utils.keys([1, 2, 3]));
+		test.done();
+	},
+	'empty array': function (test) {
+		test.deepEqual(utils._keys([]), []);
+		test.deepEqual(utils._keys([]), utils.keys([]));
+		test.done();
+	},
+	'object': function (test) {
+		test.deepEqual(utils._keys({a: 1, b: 2}), ['a', 'b']);
+		test.deepEqual(utils._keys({a: 1, b: 2}), utils.keys({a: 1, b: 2}));
+		test.done();
+	},
+	'empty object': function (test) {
+		test.deepEqual(utils._keys({}), []);
+		test.deepEqual(utils._keys({}), utils.keys({}));
+		test.done();
+	}
+};
